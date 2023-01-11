1b896cc28fef8aa21bfc7559423b8a347f58d065
Andrew Zigler <andrewzigler@gmail.com>
2023-01-11 12:03:59 -0800

    Bump sleep time

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,12 +2,12 @@
 
 echo "Initializing Mattermost for demo..."
 
-sleep 10
+sleep 20
 
 docker exec mattermost mmctl --local user create --username root --password mattermost --email example@test.com --system-admin --email-verified
 docker exec mattermost mmctl --local team create --display-name "Plantalytics, Inc." --name main
 docker exec mattermost mmctl --local channel create --team main --display-name "Datadog Alerts" --name datadog-alerts
-docker exec mattermost mmctl --local team users add main:datadog-alerts root
+docker exec mattermost mmctl --local team users add main root
 docker exec mattermost mmctl --local channel users add main:datadog-alerts root
 
 echo "\n  YOU CAN NOW LOG IN TO MATTERMOST:\n        username:  root\n        password:  mattermost\n"
