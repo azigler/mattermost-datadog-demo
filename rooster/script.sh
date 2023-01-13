e57f6cbdfdef577855c394e138b81a5bb80035ab
Andrew Zigler <andrewzigler@gmail.com>
2023-01-13 15:04:35 -0800

    Use LTS

--- rooster/script.sh
+++ rooster/script.sh
@@ -27,16 +27,16 @@ done
 echo "Initializing Mattermost for demo..."
 sleep 25
 
-echo "Setting up Mattermost with ...\n Team name: $team_name\n Team display name: $team_display_name\n Channel name: $channel_name\n Channel display name: $channel_display_name"
+echo -e "Setting up Mattermost with ...\n Team name: $team_name\n Team display name: $team_display_name\n Channel name: $channel_name\n Channel display name: $channel_display_name"
 docker exec mattermost mmctl --local team create --display-name $team_display_name --name $team_name
 docker exec mattermost mmctl --local channel create --team $team_name --display-name $channel_display_name --name $channel_name
 
-echo "Initializing your admin account ...\n\n User name: $user_name\n User password: $user_password\n"
+echo -e "Initializing your admin account ...\n\n User name: $user_name\n User password: $user_password\n"
 docker exec mattermost mmctl --local user create --username $user_name --password $user_password --email $user_name@$team_name.com --system-admin --email-verified
 docker exec mattermost mmctl --local team users add $team_name $user_name
 
 export MM_URL="$(gp url 8065)"
-echo "\n\n  YOU CAN NOW LOG IN TO MATTERMOST AT $MM_URL\n        username:  $user_name\n        password:  $user_password\n"
+echo -e "\n\n  YOU CAN NOW LOG IN TO MATTERMOST AT $MM_URL\n        username:  $user_name\n        password:  $user_password\n"
 
 cd data/users
 
